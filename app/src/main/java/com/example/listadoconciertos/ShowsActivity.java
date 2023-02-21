36d26dbad66afb9f201a04f121dab28ad4951fe6
Rau18 <RaulSG188@gmail.com>
2023-02-21 16:42:33 +0100

    Added shows info and started doing list view.

--- app/src/main/java/com/example/listadoconciertos/ShowsActivity.java
+++ app/src/main/java/com/example/listadoconciertos/ShowsActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ShowsActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +19,20 @@ public class ShowsActivity extends AppCompatActivity {
         actionBar.hide();
         setContentView(R.layout.shows_activity);
 
+        ShowsInfo sI = new ShowsInfo();
+        String[] cityList = sI.getCityList();
+        String[] discoList = sI.getDiscoList();
+        int[] logoList = sI.getLogoList();
 
+        List<HashMap<String, String>> hashList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < cityList.length; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listView_city", cityList[i]);
+            hm.put("listView_disco", discoList[i]);
+            hm.put("listView_logo", Integer.toString(logoList[i]));
+            hashList.add(hm);
+        }
     }
 
 }
--- app/src/main/java/com/example/listadoconciertos/ShowsInfo.java
+++ app/src/main/java/com/example/listadoconciertos/ShowsInfo.java
@@ -2,4 +2,27 @@ package com.example.listadoconciertos;
 
 public class ShowsInfo {
 
+    private String[] cityList = new String[]{
+            "Tarragona", "Múrcia", "Sevilla", "Jaén",
+    };
+
+    private String[] discoList = new String[]{
+            "Tarraco Arena Plaza", "Sala R.E.M", "Sala Fanatic", "Sala Kharma",
+    };
+
+    private int[] logoList = new int[]{
+            R.drawable.saikoshowtarragona, R.drawable.saikoshowmurcia, R.drawable.saikoshowsevilla, R.drawable.saikoshowjaen,
+    };
+
+    public String[] getCityList() {
+        return cityList;
+    }
+
+    public String[] getDiscoList() {
+        return discoList;
+    }
+
+    public int[] getLogoList() {
+        return logoList;
+    }
 }
